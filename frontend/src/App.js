c84e4a7a72f73997117c8ac58ed4b7a04bfd5216
Roeyasp <Roeyasp@DESKTOP-F6F9PEV>
2023-03-06 16:41:28 +0200

    add Backdrop, Sidedrawer components and complete Navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 //Screens
@@ -8,14 +9,17 @@ import HomeScreen from "./screens/HomeScreen";
 
 //Components
 import Navbar from "./components/Navbar";
+import Backdrop from "./components/Backdrop";
+import SideDrawer from "./components/SideDrawer";
 
 function App() {
+  const [sideToggle, setSideToggle] = useState(false);
+
   return (
     <Router>
-      <Navbar />
-      {/* SideDrawer */}
-      {/* Backdrop */}
-
+      <Navbar click={() => setSideToggle(true)} />
+      <Backdrop show={sideToggle} click={() => setSideToggle(false)} />
+      <SideDrawer show={sideToggle} click={() => setSideToggle(false)} />
       <main className="app">
         <Routes>
           <Route exact path="/" element={<HomeScreen />} />
